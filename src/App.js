99ba1b93963ce9f439681ea493979485ff3ad816
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2020-01-15 19:39:09 +0000

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import welcomeIcon from './assets/welcome.jpg';
+import TabsIndex from './components/tabs';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <img src={welcomeIcon} alt="Welcome!"/>
+      <TabsIndex/>
+      </div>
     );
   }
 }
---
+++ src/components/tabs.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+
+const TabComponent = () => {
+
+  return(
+    <>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+          <Tab>Title 3</Tab>
+        </TabList>
+
+      <TabPanel>
+        <h2>Any content 1</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>Any content 2</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>Any content 3</h2>
+      </TabPanel>
+      </Tabs>
+    </>
+  )
+}
+
+
+export default TabComponent;
