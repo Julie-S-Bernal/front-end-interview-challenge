3b353a7439f7c75f56dd4ba381467a8ade1723b0
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2020-01-19 14:21:16 +0000

    made row only render once

--- src/components/PaymentTable.js
+++ src/components/PaymentTable.js
@@ -4,6 +4,7 @@ import React, {useState} from 'react';
 const PaymentTable = (props) => {
 
   const [isTransactionVisible, setTransactionVisible] = useState(false);
+  const [selectedId, setSelectedId] = useState('')
 
   const setPaymentStatus = (id, isBill ) => {
     return fetch(`http://localhost:3002/bills/${id}`, {
@@ -21,6 +22,7 @@ const PaymentTable = (props) => {
   }
 
   const onRowClicked = (id, transactions)=> {
+    setSelectedId(id)
     isTransactionVisible ? setTransactionVisible(false) : setTransactionVisible(true)
   }
 
@@ -32,30 +34,30 @@ const PaymentTable = (props) => {
        if (bills.isBill && props.billsOnly) {
        return (
            <>
-          <tr id='parentRow' key={id} onClick={() => onRowClicked(id, isBill)}>
+          <tr id='parentRow' key={id} onClick={() => onRowClicked(id, bills.transactions)}>
              <td ><img alt='icon' height='50px' src={iconUrl}/></td> {/* Some images link are broken and needs fixing */}
              <td>{categoryId}</td>
              <td>{`${name}(${bills.transactions.length})`}</td>
              <td><button onClick={() => onButtonClicked(id, isBill)}>Remove bill</button></td>
           </tr>
           {isTransactionVisible ?
-          <tr>
-              <thead>
                 <tr>
-                  <th>Amount</th>
-                  <th>Date</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  {bills.transactions.map((bill, index, i) => {
-                    return (<div key={i}><td>{bill.amount}</td> <td>{bill.date}</td></div>)
-                  })}
-                </tr>
-              </tbody>
-            </tr>
-            : null }
-       </>
+                <thead>
+                  <tr>
+                    <th>Amount</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    {bills.transactions.map((bill, index, i) => {
+                      return (<div key={i}><td>{bill.amount}</td> <td>{bill.date}</td></div>)
+                    })}
+                  </tr>
+                </tbody>
+            </tr> :
+            null}
+            </>
        )
        } else {
         if (!bills.isBill && !props.billsOnly) {
@@ -78,7 +80,10 @@ const PaymentTable = (props) => {
                 <tbody>
                   <tr>
                     {bills.transactions.map((bill, index, i) => {
+                      if(id === selectedId) {
                       return (<div key={i}><td>{bill.amount}</td> <td>{bill.date}</td></div>)
+                      }
+                      return null
                     })}
                   </tr>
                 </tbody>
